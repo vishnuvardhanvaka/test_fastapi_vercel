afaa63b45e80de0b62e623223462f689a0687aff
vishnu <vishnu.vaka@solomonsint.com>
2024-03-04 21:57:14 +0530

    streaming

--- app/api.py
+++ app/api.py
@@ -24,6 +24,7 @@ origins = [
 ]
 
 app=FastAPI()
+
 app.add_middleware(
     CORSMiddleware,
     allow_origins=origins,
@@ -31,6 +32,65 @@ app.add_middleware(
     allow_methods=["*"],
     allow_headers=["*"],
 )
+
+import subprocess
+import threading
+
+class StreamProcess:
+    def __init__(self, video_path, stream_key):
+        self.video_path = video_path
+        self.stream_key = stream_key
+        self.process = None
+        self.stop_event = threading.Event()
+
+    def start(self):
+        args = [
+          '-stream_loop', '-1',
+          '-re',
+          '-i', self.video_path,
+          '-c', 'copy',
+          '-b:v', '6800k',  # Set the video bitrate to 6800 Kbps
+          '-f', 'flv',
+          '-fflags', 'nobuffer',
+          '-flags', 'low_delay',
+          f'rtmp://a.rtmp.youtube.com/live2/{self.stream_key}'
+      ]
+
+        self.process = subprocess.Popen(['ffmpeg'] + args, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    def check_stream(self):
+      if self.process and self.process.poll() is None:
+        # print("Streaming process running")
+        return True
+      # print("Streaming process stopped")
+      return False
+    def stop(self):
+        if self.process and self.process.poll() is None:
+            self.stop_event.set()  # Signal the thread to stop
+            self.process.terminate()  # Terminate the ffmpeg process
+
+def stream_video(video_path, stream_key):
+    stream_process = StreamProcess(video_path, stream_key)
+    stream_thread = threading.Thread(target=stream_process.start)
+    stream_thread.start()
+    return stream_process
+
+stream_process = None
+
+@app.get('/startStreaming')
+async def startStreaming():
+    global stream_process
+    # video_path='./finalVideo.mp4'
+    video_path = './finalVideo.mp4'
+    stream_key='68p2-txmk-79sm-hjfh-2d26'
+    stream_process = stream_video(video_path,stream_key)
+    return {"stream_status":stream_process.check_stream()}
+
+@app.get('/stopStreaming')
+async def stopStreaming():
+    global stream_process
+    if stream_process:
+        stream_process.stop()
+    return {"stream_status":stream_process.check_stream()}
 
 
 class Database:
@@ -295,6 +355,7 @@ def prNewsWire(category=''):
 # pr_news_data=prNewsWire('food')
 # print(pr_news_data)
 # email:str=Form(...)
+
 @app.post('/getNews/')
 async def getNews(category: Optional[str] = Form(None), date: Optional[str] = Form(None)):
     print(category, date, type(category), type(date))
